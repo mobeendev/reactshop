bc53fe16c4401f49129b0ef0b7d1d9a0dec2e3f3
Abdul Mobeen <mobeen@alkunat.com>
2019-12-14 13:37:46 +0300

    added route to menu items --v1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+// title, imageUrl, size, history, linkUrl, match 
+const MenuItem = ({ title, imageUrl, size,linkUrl,match,history }) => {
+  
+  // console.log(linkUrl);
+  // console.log(history);
+  
+  return (    
+    
+    <div className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+    
+    
     <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
+    className='background-image'
+    style={{
+      backgroundImage: `url(${imageUrl})`
+    }}
     />
     <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <span className='subtitle'>SHOP NOW</span>
     </div>
-  </div>
-);
-
-export default MenuItem;
+    </div>
+    );
+  }
+    
+  export default withRouter(MenuItem);
+    
