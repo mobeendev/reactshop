9eb568ee7419494eaaae1efaa4b47986962ec956
Abdul Mobeen <mobeen@alkunat.com>
2019-12-14 15:49:56 +0300

    Added collections area

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h2>Hats page</h2>
-  </div>
-);
-
-
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
