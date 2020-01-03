a135ad11ad413f79cafb33d176dc5d816a6d20e5
Abdul Mobeen <mobeen@alkunat.com>
2020-01-03 14:53:26 +0300

    Added Signin Form

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,12 +11,18 @@ const Header = () => (
       <Logo className='logo' />
     </Link>
     <div className='options'>
+      <Link className='option' to='/'>
+        HOME
+      </Link>
       <Link className='option' to='/shop'>
         SHOP
       </Link>
       <Link className='option' to='/shop'>
         CONTACT
       </Link>
+      <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
     </div>
   </div>
 );
