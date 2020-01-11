70f9680fdbcb1dd56f28f75d77049f68cda7da93
Abdul Mobeen <mobeen@alkunat.com>
2020-01-11 14:37:36 +0300

    integrated redux into application & added redirect

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
     </Link>
     <div className='options'>
-      <Link className='option' to='/'>
-        HOME
-      </Link>
       <Link className='option' to='/shop'>
         SHOP
       </Link>
@@ -35,4 +33,8 @@ const Header = ({currentUser}) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
