d1040c464fec32d9577df008b55b32f6574f56c5
Abdul Mobeen <mobeen@alkunat.com>
2020-01-26 23:04:10 +0300

    Updates

---
+++ precache-manifest.60a653369aba626b424aac24207fadbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/reactshop/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "5f83912524c33d184191",
+    "url": "/reactshop/static/js/runtime~main.8c2e8932.js"
+  },
+  {
+    "revision": "1b287747fd797f0ff33b",
+    "url": "/reactshop/static/js/main.ee55870b.chunk.js"
+  },
+  {
+    "revision": "f9de6cf829d215a83d57",
+    "url": "/reactshop/static/js/2.1df76050.chunk.js"
+  },
+  {
+    "revision": "1b287747fd797f0ff33b",
+    "url": "/reactshop/static/css/main.7081d58d.chunk.css"
+  },
+  {
+    "revision": "1f9f0d0ce5aa424a3f3fceca335f3ea2",
+    "url": "/reactshop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mobeendev/reactshop/precache-manifest.30cea0678f184e7886022bfa8d3f3065.js"
+  "/reactshop/precache-manifest.60a653369aba626b424aac24207fadbd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/mobeendev/reactshop/index.html", {
+workbox.routing.registerNavigationRoute("/reactshop/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
