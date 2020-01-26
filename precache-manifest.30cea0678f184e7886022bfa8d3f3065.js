43695d50c9182c1975237a9ad36240d3ac469bc6
Abdul Mobeen <mobeen@alkunat.com>
2020-01-26 22:57:15 +0300

    Updates

---
+++ precache-manifest.30cea0678f184e7886022bfa8d3f3065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/mobeendev/reactshop/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "f0df466e84f5acab0f8e",
+    "url": "/mobeendev/reactshop/static/js/runtime~main.e2ea34bb.js"
+  },
+  {
+    "revision": "1b287747fd797f0ff33b",
+    "url": "/mobeendev/reactshop/static/js/main.ee55870b.chunk.js"
+  },
+  {
+    "revision": "886000774a3f95813afd",
+    "url": "/mobeendev/reactshop/static/js/2.cc5126f3.chunk.js"
+  },
+  {
+    "revision": "1b287747fd797f0ff33b",
+    "url": "/mobeendev/reactshop/static/css/main.7081d58d.chunk.css"
+  },
+  {
+    "revision": "2e90759b7eb00177d4d54e05ebc3d88c",
+    "url": "/mobeendev/reactshop/index.html"
+  }
+];
